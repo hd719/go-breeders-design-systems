37f3246de11a863a05b73f4a0b704a1b51eb7b5d
hd719 <hameldesai93@gmail.com>
2024-05-06 13:02:35 -0400

    section-8-video-66

---
+++ cmd/web/adapters.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"go-breeders/models"
+	"io"
+	"net/http"
+)
+
+type CatBreedsInterface interface {
+	GetAllCatBreeds() ([]*models.CatBreed, error)
+}
+
+type RemoteService struct {
+	Remote CatBreedsInterface
+}
+
+func (rs *RemoteService) GetAllBreeds() ([]*models.CatBreed, error) {
+	return rs.Remote.GetAllCatBreeds()
+}
+
+type JSONBackend struct{}
+
+func (jd *JSONBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	resp, err := http.Get("http://localhost:8081/api/cat-breeds/json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	var breeds []*models.CatBreed
+	err = json.Unmarshal(body, &breeds)
+	if err != nil {
+		return nil, err
+	}
+
+	return breeds, nil
+}
