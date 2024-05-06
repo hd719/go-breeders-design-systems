d811d7ab02cb81b7024d5362f43564f736f150a8
hd719 <hameldesai93@gmail.com>
2024-05-06 13:02:35 -0400

    section-4-video-43

---
+++ cmd/web/setup_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"go-breeders/models"
+	"log"
+	"os"
+	"testing"
+)
+
+var testApp application
+
+func TestMain(m *testing.M) {
+	dsn := "mariadb:myverysecretpassword@tcp(localhost:3306)/breeders_design_systems?parseTime=true&tls=false&collation=utf8_unicode_ci&timeout=5s"
+	db, err := initMySQLDB(dsn)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	testApp = application{
+		DB:     db,
+		Models: *models.New(db),
+	}
+
+	os.Exit(m.Run())
+}
